0746fe0b253133f27736bc9fa53e4c5e4e25189a
kaisnowwolfhan <showlom5403@gmail.com>
2026-07-15 01:40:00 +0800

    content: 人工智慧導論文本修復; core: plugins update

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,9 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
+// --- 中文 Graph 修正補丁：自動解碼局部圖表的網址 ---
+
+// ------------------------------------------------
+
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
